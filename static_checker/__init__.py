0ab7537827dd48caab70a49f21a32f05c41ec69d
toejough <toejough@gmail.com>
2016-08-18 12:47:24 -0400

    moving vcs stuff into its own module

--- static_checker/__init__.py
+++ static_checker/__init__.py
@@ -9,10 +9,10 @@ from typing import Sequence
 # [ -Project ]
 from .components.checker import Checker
 from .components.watcher import Watcher
-from .interactors.runner import run
-from .thin_types.command import Command
+from .components.source_control import commit
 from .interactors import config
 from .interactors import cli
+from .thin_types.command import Command
 
 
 # [ Logging ]
@@ -24,27 +24,6 @@ logger = logging.getLogger(__name__)
 # XXX print the file being checked
 # XXX Bump logging to warning
 # XXX add unit tests
-# [ Helper helpers ]
-def get_status() -> Sequence[str]:
-    """Get repo status."""
-    logger.info('getting status...')
-    return run('git', ['status', '--porcelain']).output.splitlines()
-
-
-def add_missing_paths(paths: Sequence[Path]) -> None:
-    """Add any missing paths to the repo."""
-    logger.info('adding missing paths...')
-    status_lines = get_status()
-    if not status_lines:
-        return
-    new_file_lines = [l for l in status_lines if l.startswith('??')]
-    new_path_strings = [v for l in new_file_lines for k, v in [l.split()]]
-    for path in paths:
-        path_string = str(path)
-        if path_string in new_path_strings:
-            run('git', ['add', path_string])
-
-
 # [ Helpers ]
 def get_paths() -> Sequence[Path]:
     """Return the paths to watch/check."""
@@ -55,19 +34,6 @@ def get_paths() -> Sequence[Path]:
 def get_commands() -> Sequence[Command]:
     """Return the commands to run."""
     return [Command(*c) for c in config.get_config('checks')]
-
-
-def commit(paths: Sequence[Path]) -> None:
-    """Commit the current repo."""
-    logger.info('committing...')
-    add_missing_paths(paths)
-    status_lines = get_status()
-    modified_file_lines = [l for l in status_lines if not l.startswith('??')]
-    if not modified_file_lines:
-        return
-    print(run('git', ['diff', '--color']).output)
-    message = input('commit message: ')
-    run('git', ['commit', '-am', message])
 
 
 # [ Main ]
