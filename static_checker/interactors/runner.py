8744b6538fd2e5cb97ff0cb617cd8ba1042f2d9e
toejough <toejough@gmail.com>
2016-08-19 01:42:59 -0400

    updated to asyncio subprocess

--- static_checker/interactors/runner.py
+++ static_checker/interactors/runner.py
@@ -6,9 +6,8 @@
 import subprocess
 from types import SimpleNamespace
 from typing import Sequence
+import asyncio
 import logging
-# [ -Third Party ]
-import a_sync
 
 
 # [ Logging ]
@@ -17,17 +16,16 @@ logger = logging.getLogger(__name__)
 
 # [ Helpers ]
 # XXX better doc strings
-# XXX use asyncio subprocess
 # XXX add unit tests
 async def run(command: str, args: Sequence[str]) -> SimpleNamespace:
     """Run the command."""
     logger.info("Running command: {} {}".format(command, ' '.join(args)))
-    result = await a_sync.run(
-        subprocess.run,
-        [command, *args],
+    process = await asyncio.create_subprocess_exec(
+        command, *args,
         stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
     )
+    await process.wait()
     return SimpleNamespace(
-        success=result.returncode == 0,
-        output=str(result.stdout, 'utf-8')
+        success=process.returncode == 0,
+        output=str(await process.stdout.read(), 'utf-8')
     )
