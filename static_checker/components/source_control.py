7eeb0250b9617ece6657927aefa910bd8509fd88
toejough <toejough@gmail.com>
2016-08-19 01:06:02 -0400

    updating source_control async calls

--- static_checker/components/source_control.py
+++ static_checker/components/source_control.py
@@ -26,7 +26,7 @@ async def _get_status() -> Sequence[str]:
     return (await run('git', ['status', '--porcelain'])).output.splitlines()
 
 
-def _add_missing_paths(paths: Sequence[Path], status_lines: Sequence[str]) -> None:
+async def _add_missing_paths(paths: Sequence[Path], status_lines: Sequence[str]) -> None:
     """Add any missing paths to the repo."""
     logger.info('adding missing paths...')
     if not status_lines:
@@ -36,7 +36,7 @@ def _add_missing_paths(paths: Sequence[Path], status_lines: Sequence[str]) -> No
     for path in paths:
         path_string = str(path)
         if path_string in new_path_strings:
-            run('git', ['add', path_string])
+            await run('git', ['add', path_string])
 
 
 # [ API ]
@@ -44,11 +44,11 @@ async def commit(paths: Sequence[Path]) -> None:
     """Commit the current repo."""
     logger.info('committing...')
     status_lines = await _get_status()
-    _add_missing_paths(paths, status_lines)
+    await _add_missing_paths(paths, status_lines)
     modified_file_lines = [l for l in status_lines if not l.startswith('??')]
     if not modified_file_lines:
         return
-    await a_sync.run(print, (await run('git', ['diff', '--color'])).output)
+    print((await run('git', ['diff', '--color'])).output)
     try:
         message = await a_sync.run(input, 'commit message: ')
     except concurrent.futures.CancelledError:
